54696b895cca62b4770b278ff1b80a453582ebdd
Maxim <maxion1234@gmail.com>
2023-11-27 15:38:44 -0500

    Adds to README, adds registration screen

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
-import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    return render_template('index.html', current_time=current_time)
+    return render_template('index.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
