6aa15787eb363c40153e84521807b4d970aa1330
ryanmj14 <ryanjones1417@gmail.com>
2023-11-29 13:39:34 -0500

    updated app.py with database configuration

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 from flask import Flask, render_template
-import datetime
+from flask_sqlalchemy import SQLAlchemy
+from src.blueprints.auth import auth_bp
+from extensions import db
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5434/database_name'
+db.init_app(app)
+
+app.register_blueprint(auth_bp)
+
+#TO DO - deal with routes - might only need the one for index.html in this file
+
 @app.route('/')
 def index():
-    current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    return render_template('index.html', current_time=current_time)
+    return render_template('index.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/forum')
+def forum():
+    return render_template('forum.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
